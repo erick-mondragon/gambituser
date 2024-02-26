ee96fda9c2deefe468aa782f3b8ca3a8afe6e91b
unknown <erickmmondragon@gmail.com>
2024-02-26 00:45:10 -0500

    Conexión BD y Signup users

---
+++ db/signup.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"gambituser/models"
+	"gambituser/tools"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySQL() + "')"
+	fmt.Println(sentencia)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Signup > Ejecución Exitosa")
+	return nil
+
+}
